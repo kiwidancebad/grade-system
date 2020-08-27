b00a502cb8bb722b445607177e3853d8e113439b
kiwidancebad <morelessequal@gmail.com>
2020-08-27 23:38:02 +0300

    mocks

---
+++ src/mock.ts
@@ -0,0 +1,28 @@
+import { ITech } from 'DataTypes';
+
+export const NODES_DATA_MOCK: ITech[] = [
+  {
+    id: '0',
+    name: 'JavaScript',
+    skills: [
+      {
+        id: 'variables',
+        name: 'variables',
+        description: 'javascript variables',
+        links: []
+      },
+      {
+        id: 'scope',
+        name: 'scope',
+        description: 'javascript scope',
+        links: []
+      },
+      {
+        id: 'functions',
+        name: 'functions',
+        description: 'javascript functions',
+        links: []
+      }
+    ]
+  }
+];
--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,5 +1,17 @@
 declare module 'DataTypes' {
   export interface ISkill {
+    id: string;
     name: string;
+    description: string;
+    links: {
+      name?: string;
+      uri: string;
+    }[];
+  }
+
+  export interface ITech {
+    id: string;
+    name: string;
+    skills: ISkill[];
   }
 }
